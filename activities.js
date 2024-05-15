b554bb67d558bb73d146c716319d299dee337289
prodanovicaleksej <161127701+prodanovicaleksej@users.noreply.github.com>
2024-05-15 19:49:48 +0200

    Add custom JSON files

---
+++ activities.js
@@ -0,0 +1,170 @@
+const activities = [
+  {
+    activity: "Paddle Boarding",
+    description:
+      "Ova aktivnost je dio ture uz obalu i zabavna je za goste svih uzrasta. Aktivnost stajanja na dasci za veslanje (Paddle Boarding) dio je paketa  i opuštena je i zabavna aktivnost za sve. Održava se na dan dolaska gostiju u obalnu kuću, nakon što su uživali u dobrodošlom ručku. Stajanje na dasci za veslanje (SUP) najbrže je rastući vodeni sport. Zabavan je, lako se uči i održava vas u formi. Bez obzira je li vam prvi put na dasci ili ste iskusni veslač, možemo vas opremiti najboljom opremom i odvesti vas na avanturu SUP-om.",
+
+    cijena: "70€",
+    img: "img/paddeling.jpg",
+    galerija: "img/paddeling1.jpg",
+    galerija2: "img/paddeling2.jpg",
+    galerija3: "img/paddeling3.jpg",
+  },
+
+  {
+    activity: "Mountaineering",
+    description:
+      "Planinarenje i alpinizam su dva sporta koja dijele slične karakteristike, međutim, kao općenito pravilo, planinarenje je aktivnost koju možete pratiti bez potrebe za bilo kojom specijalnom ili tehničkom opremom, dok će alpinizam zahtijevati uporabu tehničke opreme poput užeta, pojasa, dereza i eventualno ledenih sekira. Alpinizam je fizički izazovniji sport i određena razina znanja i obuke trebala bi se steknuti prije pokušaja bilo kakvih teških planinarskih tura. To znanje može uključivati navigaciju, tehnike vezanja užeta, upotrebu dereza i ledenih sekira, osnove prve pomoći i razumijevanje vremenskih uvjeta u planinama. Alpinizam nudi neograničene mogućnosti za izazove, užitak i uzbuđenje!",
+
+    cijena: "100€",
+    img: "img/penjanje.jpg",
+    galerija: "img/penjanje1.jpg",
+    galerija2: "img/penjanje2.jpg",
+    galerija3: "img/penjanje3.jpg",
+  },
+
+  {
+    activity: "Jeep Safari",
+    description:
+      "Ova tura džipom je opuštena aktivnost puna uzbuđenja i naziva se  jer je to pravi potpuni krug oko veličanstvenih Durmitorskih planina. Dužina ture je 85 km. Tura traje između 4-5 sati s povratkom u naš kompleks. Ruta vodi goste kroz planine Durmitora za opuštenu aktivnost koja obuhvaća najbolje prirodne ljepote i krajolike područja. Duž puta ima mnogo stajanja za goste da naprave nevjerojatne i zadivljujuće fotografije. Tura džipom će naše goste odvesti preko panorama Zabljaka, na prosječnoj visini od 1800 m nadmorske visine.",
+    cijena: "200€",
+    img: "img/jeep.jpg",
+    galerija: "img/jeep1.jpg",
+    galerija2: "img/jeep2.jpg",
+    galerija3: "img/jeep3.jpg",
+  },
+
+  {
+    activity: "Lakes & Canyon Tours",
+    description:
+      "Ova aktivnost je savršen način za opuštanje i uživanje u Durmitoru. Tura obuhvaća brojne zanimljive točke, pružajući gostima priliku da istraže ljepote ovog područja. Počinje s opcionalnom premium aktivnošću zip-lininga, nakon čega slijede ugodne šetnje oko dva jezera, uz mogućnost plivanja za zainteresirane goste. Jezera su udaljena oko 20 km od grada Zabljaka. Nakon toga, gosti uživaju u tradicionalnom crnogorskom ručku, a potom slijedi posjet kanjonu Curevac s nevjerojatnim pogledima. Cijela aktivnost traje oko 5-6 sati.",
+    cijena: "100€",
+    img: "img/lake.jpg",
+    galerija: "img/lake1.jpg",
+    galerija2: "img/lake2.jpg",
+    galerija3: "img/lake3.jpg",
+  },
+
+  {
+    activity: "Rafting Tours",
+    description:
+      "Spust niz čiste vode rijeke Tare je sjajno iskustvo koje svaki gost treba probati! Na visini od 1300 metara, Tara kanjon je drugi najdublji kanjon na svijetu, iza samo Velikog kanjona u SAD-u! Tara rijeka je samo kratkih 25 minuta vožnje udaljena od kampa i upisana je na UNESCO-ov popis svjetske baštine, s raftingom na Tari kao jednom od najpopularnijih turističkih aktivnosti za sve posjetitelje Crne Gore. Aktivnost raftinga uključuje svu visokokvalitetnu opremu, koja je dizajnirana za ekstremno rafting (neoprenska odijela, neoprenske čizme, pojas za spašavanje, kacige). Tara rijeka je najatraktivniji kanjon u Europi i bogata je prirodom; s prekrasnim slapovima, divljim pritokama i gustim šumama! Aktivnost raftinga je predivno iskustvo koje će ostati s svim našim gostima zauvijek, stoga skočite na brod i pridružite se brzacima za odlično vrijeme!",
+    cijena: "200€",
+    img: "img/rafting.jpg",
+    galerija: "img/rafting1.jpg",
+    galerija2: "img/rafting2.jpg",
+    galerija3: "img/rafting3.jpg",
+  },
+
+  {
+    activity: "Boka Bay Tour",
+    description:
+      "Ovo je opuštena aktivnost koju naši gosti mogu odabrati, jer je to divan izlet na obalu Crne Gore koji obuhvaća čudesni zaljev Kotora. Ova aktivnost pružila bi gostima fantastične krajolike i povijest, posjećujući povijesne gradove u zaljevu. Obalni obilazak uključuje: Grad Perast - Kotor - Tivat (Porto Montenegro) - Morinj. Za ovu aktivnost potrebno je minimalno 6 osoba.",
+    cijena: "390€",
+    img: "img/boka.jpg",
+    galerija: "img/boka1.jpg",
+    galerija2: "img/boka2.jpg",
+    galerija3: "img/boka3.jpg",
+  },
+
+  {
+    activity: "Canyon Nevidio",
+    description:
+      "Nevidio kanjon se nalazi u centralnom dijelu Crne Gore, na južnim padinama Durmitora. Kroz kanjon protiče rijeka Mala Komarnica koja izvire na području Dobrog doa. Nevidio kanjon zaista nosi svoje ime, jer se rijeka Mala Komarnica gubi nakon ulaska u kanjon, u čudesno izvajanom ulazu koji postaje nevidljiv ljudskom oku. Nevidio kanjon predstavlja vrstu galerije prirode čije skulpture oblikuju stijene i voda. /n Nevidio kanjon je dug oko 2 kilometra, s razlikom u nadmorskoj visini od 125 metara. Stijene kanjona dosežu visinu od 450 metara, a u nekim područjima širine su samo pola metra. Ulaz u kanjon nalazi se na nadmorskoj visini od 1050 metara. Jedna od najboljih atrakcija ovog područja je selo Pošćenje, koje ima dva ledenjačka jezera i periodični vodopad zvan Skakavica - koji razbacuje vodu rijeke Grabovice u kanjon s stijene visoke sedamdeset metara. Danas Nevidio kanjon predstavlja jedinstvenu turističku atrakciju, s prekrasnom klisurom, pjenušavim i svjetlucavim valovima, kaskadama i bazenima koji su dovoljni razlozi za posjet kanjonu!",
+    cijena: "100€",
+    img: "img/nevidio.jpg",
+    galerija: "img/nevidio1.jpg",
+    galerija2: "img/nevidio2.jpg",
+    galerija3: "img/nevidio3.jpg",
+  },
+
+  {
+    activity: "Adrenaline Zip line",
+    description:
+      "Uzbudljiva avantura zip linijom duž rijeke Tare! Zip linija, koja se proteže pokraj mosta Đurđevića Tara, pruža nezaboravno iskustvo visokog adrenalina. Sa svojih 824 metra duljine i visine od oko 180 metara, ova zip linija je najduža u Europi. Zaokupljeni uzbuđenjem i osjećajem slobode, sudionici prolaze kroz nevjerojatne pejzaže kanjona rijeke Tare. Putovanje zip linijom traje otprilike 2-3 minute, tijekom kojih se pružaju zadivljujući panoramski pogledi. Ova adrenalinska aktivnost jamči doživljaj koji će se dugo pamtiti! Ubrzani povjetarac, osjećaj slobode i pogledi koji oduzimaju dah - sve to čeka hrabre avanturiste na zip liniji preko kanjona rijeke Tare. Dok se spuštaju niz najdužu zip liniju u Europi, dugačku 824 metra i visoku oko 180 metara, sudionici doživljavaju nevjerojatnu dozu adrenalina. Ova adrenalinska avantura traje oko 2-3 minute, ali ostavlja trajne uspomene zahvaljujući spektakularnim pejzažima koji se pružaju ispred njih. Nema ništa poput osjećaja letenja iznad netaknute prirode kanjona Tare - doživljaj koji se ne zaboravlja!",
+    cijena: "20€",
+    img: "img/zip.jpg",
+    galerija: "img/zip1.jpg",
+    galerija2: "img/zip2.jpg",
+    galerija3: "img/zip3.jpg",
+  },
+
+  {
+    activity: "Horse riding",
+    description:
+      "Ova uzbudljiva aktivnost jahanja konja nudi nezaboravno iskustvo u prirodi. Bez obzira jeste li potpuni početnik ili već iskusni jahač, ovo je prilika da se prepustite čaroliji jahanja kroz prekrasne krajolike. Jahanje konja je jedinstveno iskustvo koje će osvojiti srca svih uzrasta. Bilo da se radi o opuštenoj vožnji ili avanturi u prirodi, ova aktivnost će biti vrhunac tjedna za sve koji se odluče sudjelovati. Zvuk kopita dok se lagano krećete kroz šumu ili osjećaj slobode dok prelazite prekrasne livade - to su samo neki od trenutaka koje možete doživjeti jahaći konja. Ova aktivnost nije samo uzbudljiva, već i terapeutski opuštajuća. Za djecu i odrasle, jahanje konja pruža priliku za povezivanje s prirodom na jedinstven način. Neovisno o tome tražite li avanturu ili samo trenutke mira, ova aktivnost će vam pružiti nezaboravan doživljaj koji će obogatiti vašu pustolovinu.",
+    cijena: "20€",
+    img: "img/horse.jpg",
+    galerija: "img/horse1.jpg",
+    galerija2: "img/horse2.jpg",
+    galerija3: "img/horse3.jpg",
+  },
+
+  {
+    activity: "Trekking tours",
+    description:
+      "Pješačke aktivnosti uključuju jednodnevne ili višednevne ture duž planina i ruralnih područja u Durmitoru. Za višednevne pješačke aktivnosti, to može uključivati noćenje u prirodi, s prenoćištima organiziranim u šatorima ili ljetnim planinskim kućicama. Ovisno o trajanju pješačke aktivnosti, moguće je posjetiti cijelo područje i planinske lance. Jedna od sjajnih strana pješačenja je što zahtijeva minimalnu opremu, a zauzvrat pruža užitak u svakom koraku koji napravite! Kao pravilo, za pješačenje nije potrebno posebno tehničko znanje, ali udobne cipele za hodanje, odgovarajuća oprema i, ovisno o odredištu, dobra fizička kondicija su potrebni. Mogućnosti odabira odredišta su zaista velike i ovise o vašem raspoloženju i trenutnim mogućnostima. Bez obzira na to koju pješačku turu naši gosti odaberu, najvažniji aspekt bit će njihova interakcija s prirodom i uživanje u svježem planinskom zraku!",
+    cijena: "50€",
+    img: "img/pjeske.jpg",
+    galerija: "img/pjeske1.jpg",
+    galerija2: "img/pjeske2.jpg",
+    galerija3: "img/pjeske3.jpg",
+  },
+
+  {
+    activity: "Mountain Bike Activity",
+    description:
+      "Ruta biciklizma usredotočena je na uživanje u prirodi, svježem zraku i suncu, a manje na postizanje brzinskih rekorda. Putovanje prolazi kroz različita područja uključujući Moticiki Gaj, Zabljak i okolna seoska područja Durmitora. Traje otprilike 4,5-5,5 sati s učestalim pauzama. Pogledi na planine i prirodu tijekom vožnje su spektakularni. Također, dostupna je i dvodnevna aktivnost planinskog biciklizma koja obuhvaća rutu Oko Durmitora, s noćenjem u selu Trsa. Ova aktivnost planinskog biciklizma pruža nezaboravno iskustvo istraživanja netaknute prirode Durmitora na dva kotača. Vožnja kroz raznolike krajolike omogućuje gostima da u potpunosti dožive ljepotu i raznolikost ovog područja. Bez obzira jeste li zaljubljenik u aktivni odmor ili jednostavno želite uživati u opuštenoj vožnji kroz prirodu, ova aktivnost nudi nešto za svakoga. Dvodnevna ruta Oko Durmitora pruža dublji uvid u ljepotu ovog područja, omogućavajući gostima da istraže šume, planine i ravnice Durmitora te da dožive lokalni način života kroz noćenje u selu Trsa.",
+    cijena: "50€",
+    img: "img/biciklo.jpg",
+    galerija: "img/biciklo1.jpg",
+    galerija2: "img/biciklo2.jpg",
+    galerija3: "img/biciklo3.jpg",
+  },
+
+  {
+    activity: "Kite Surfing",
+    description:
+      "Iznenađenje za vaša čula jer plaža Ada Bojana je izuzetno mesto za boravak uz obalu! Smestena u zasticenom prirodnom podrucju, okružena dinama, aluvijalnom šumom i močvarama i sa pješčanom četrnaest kilometarskom plažom, savršeno skriva nevjerojatno mesto gde provesti letnji odmor. Područje je prepuno legendi, neispričanih priča i skrivenih mesta za istraživanje, pa gosti jednostavno treba da uzmu SUP, bicikl, dugačku dasku ili zmaja da bi se potpuno uronili u istraživanje!Kite-surfing nije težak za naučiti, jednostavno zamislite sebe kako sedite na plaži i gledate nekoga kako surfuje, izgleda tako prirodno da automatski počnete zamišljati sebe kako to radite. U vašem umu prolazite kroz pokrete, osećate vodu, vetar i to pokreće vaše telo da se pomera, od misli Mogu, mogu da to uradim do Voleo bih to da uradim! Kite-surfing instruktori su pravi profesionalci i garantuju da će gosti biti na dasci tokom svog trodnevnog kursa! Deca su najbolja u učenju kako da kite-surfuju i zbog njih će sport kite-boarding biti uključen na Olimpijskim igrama mladih 2018. To je sjajan sport za upoznavanje dece sa kite-surfingom i svim njegovim prednostima i zdravim načinom života. Svi instruktori za decu poseduju IKO licence.",
+    cijena: "150€",
+    img: "img/kite.jpg",
+    galerija: "img/kite1.jpg",
+    galerija2: "img/kite2.jpg",
+    galerija3: "img/kite3.jpg",
+  },
+
+  {
+    activity: "Sea Kayaking",
+    description:
+      "Sjajan način da doživite obalu Crne Gore je naravno na morskoj vodi, a jedna od aktivnosti je kajak na moru! Kajakarenje na moru je sjajno za ljude da se opuste i uživaju u danu kajakarenja u prelepom Bokokotorskom zalivu sa licenciranim vodičima. Kajakarenje na moru može biti i zahtevan sport i odličan način opuštanja, upijanja pejzaža i dubokog udisanja morskog vazduha. Veslanjem preko zaliva možete istražiti njegovu lepotu iz različite perspektive i otkriti slikovito ribarsko selo Rose na poluostrvu Luštici. Gosti će se zabaviti i otkriti šareni morski život i osetiti bogatu istoriju crnogorske obale. Aktivnost je poludnevna tura i sjajna je za istraživanje tipičnog crnogorskog sela Rose, koje je osvojilo nagradu Divlja lepota 2013. sa pohvalama za najbolju turističku destinaciju u Crnoj Gori. Grad se nalazi nasuprot Herceg Novom duž poluostrva Luštice. Profesionalni licencirani vodiči koriste svu profesionalnu opremu: jedno i dvosedne kajake, prsluke za spasavanje, vesla, suve vreće - itd.",
+    cijena: "100€",
+    img: "img/kayak.jpg",
+    galerija: "img/kajak1.jpg",
+    galerija2: "img/kajak2.jpg",
+    galerija3: "img/kajak3.jpg",
+  },
+
+  {
+    activity: "Sailing",
+    description:
+      "Jedrenje je neverovatan način da doživite Jadransku obalu, i uvek je najbolje zapravo izaći i biti deo toga, a  jedriličarska aktivnost to i mnogo više pruža, jer gosti mogu naučiti osnove jedrenja dok uživaju u sjajnom, zabavnom danu u zalivu i na otvorenom moru! Jadranska obala blagoslovena je blagom i prijatnom klimom tokom cele godine, što je čini savršenom aktivnošću od rane proleća do kasne jeseni. Jedrilica je dužine 8 metara, a gosti će naučiti osnove jedrenja ili unaprediti svoje postojeće nautičke veštine. Naši iskusni, profesionalni i prijateljski skiperi izvešće goste na 4-satno iskustvo, što je savršeno vreme provedeno na vodi. Aktivnost će gostima pružiti osnove jedrenja, a sa ovim stečenim znanjem svaki gost će imati priliku da zaista preuzme kontrolu nad jedrima! Fantastična i nezaboravna aktivnost koja jednostavno omogućava uživanje i osećanje magije jedara, pustivši vetar da nas odvede do neverovatnih vidika i prirodnih laguna!",
+    cijena: "250€",
+    img: "img/sailing.jpg",
+    galerija: "img/sailing1.jpg",
+    galerija2: "img/sailing2.jpg",
+    galerija3: "img/sailing3.jpg",
+  },
+
+  {
+    activity: "Kotor Bay Tour",
+    description:
+      " Vožnja brodom oko Bokokotorskog zaliva jedini je način da zaista doživite neverovatne pejzaže. Ovo je opuštajući kulturni izlet, i gosti samo treba da se opuste i uživaju dok putuju ispod visokih fjordova Bokokotorskog zaliva. Vožnja brodom jednostavno je savršen način da vidite i posetite neka od crnogorskih gradova koji su deo UNESCO-ve svetske baštine i uživate u pejzažu i najpoznatijim lepotama, dok izbegavate gužvu. Plovidba Bokokotorskim zalivom nudi veoma zanimljivu istoriju kao i uvid u drevnu civilizaciju. Gosti mogu videti nekoliko malih primorskih gradova koji stvaraju utisak uspavanih lepotica koje nisu izgubile ništa od svoje izuzetnosti uprkos dugom vekovnom snu. Bogati arhitektonski detalji iz davnih vremena neće vas ostaviti ravnodušnim. Smješten uz jedan od najlepših zaliva Crne Gore je Kotor, grad trgovaca i poznatih mornara, sa mnogo priča za ispričati. Stari mediteranski grad Kotor, okružen impresivnim gradskim zidom izgrađenim od strane Nemanjića, veoma je dobro očuvan i zaštićen od strane UNESCO-a. Gosti će imati priliku da sami istraže prelep Stari grad i sa našim iskusnim skiperom, gostima će jednostavno biti sjajno videti zaliv iz mora!",
+    cijena: "50€",
+    img: "img/kotor.jpg",
+    galerija: "img/kotor1.jpg",
+    galerija2: "img/kotor2.jpg",
+    galerija3: "img/kotor3.jpg",
+  },
+];
+
+export default activities;
---
+++ slike.js
@@ -0,0 +1,211 @@
+const img = [
+  {
+    img: "img/parti1.jpg",
+  },
+  {
+    img: "img/parti2.jpg",
+  },
+  {
+    img: "img/parti3.jpeg",
+  },
+  {
+    img: "img/parti4.jpg",
+  },
+  {
+    img: "img/parti5.jpg",
+  },
+  {
+    img: "img/parti6.jpg",
+  },
+  {
+    img: "img/parti7.jpg",
+  },
+  {
+    img: "img/parti8.jpg",
+  },
+
+  {
+    img: "img/parti9.jpg",
+  },
+
+  {
+    img: "img/parti10.jpg",
+  },
+  {
+    img: "img/parti11.jpg",
+  },
+  {
+    img: "img/parti12.jpg",
+  },
+  {
+    img: "img/parti13.jpg",
+  },
+  {
+    img: "img/parti14.jpg",
+  },
+  {
+    img: "img/parti15.jpg",
+  },
+  {
+    img: "img/parti16.jpg",
+  },
+  {
+    img: "img/parti17.jpg",
+  },
+  {
+    img: "img/parti18.jpg",
+  },
+  {
+    img: "img/parti19.jpg",
+  },
+  {
+    img: "img/parti20.jpg",
+  },
+  {
+    img: "img/parti21.jpg",
+  },
+  {
+    img: "img/parti22.jpg",
+  },
+  {
+    img: "img/parti23.jpg",
+  },
+  {
+    img: "img/parti24.jpg",
+  },
+  {
+    img: "img/parti25.jpg",
+  },
+  {
+    img: "img/parti26.jpg",
+  },
+  {
+    img: "img/parti27.jpg",
+  },
+  {
+    img: "img/parri28.jpg",
+  },
+  {
+    img: "img/parti29.jpg",
+  },
+  {
+    img: "img/parti30.jpg",
+  },
+  {
+    img: "img/parti31.jpeg",
+  },
+  {
+    img: "img/parti32.jpg",
+  },
+  {
+    img: "img/parti33.jpg",
+  },
+  {
+    img: "img/parti35.jpg",
+  },
+  {
+    img: "img/parti36.jpg",
+  },
+  {
+    img: "img/parti37.jpg",
+  },
+  {
+    img: "img/parti38.jpg",
+  },
+  {
+    img: "img/parti39.jpeg",
+  },
+
+  {
+    img: "img/parti40.jpg",
+  },
+  {
+    img: "img/parti41.jpg",
+  },
+  {
+    img: "img/parti42.jpg",
+  },
+  {
+    img: "img/parti43.jpg",
+  },
+  {
+    img: "img/parti44.jpg",
+  },
+  {
+    img: "img/parti45.jpg",
+  },
+  {
+    img: "img/parti46.jpg",
+  },
+  {
+    img: "img/parti47.jpg",
+  },
+  {
+    img: "img/parti48.jpg",
+  },
+  {
+    img: "img/parti49.jpg",
+  },
+  {
+    img: "img/parti50.jpg",
+  },
+  {
+    img: "img/parti51.jpg",
+  },
+  {
+    img: "img/parti52.jpg",
+  },
+  {
+    img: "img/parti53.jpg",
+  },
+  {
+    img: "img/parti54.jpg",
+  },
+  {
+    img: "img/parti55.jpg",
+  },
+  {
+    img: "img/parti56.jpg",
+  },
+  {
+    img: "img/parti57.jpg",
+  },
+  {
+    img: "img/parti58.jpg",
+  },
+  {
+    img: "img/parti59.jpg",
+  },
+  {
+    img: "img/parti60.jpg",
+  },
+  {
+    img: "img/parti61.jpg",
+  },
+  {
+    img: "img/parti62.jpg",
+  },
+  {
+    img: "img/parti63.jpg",
+  },
+  {
+    img: "img/parti64.jpg",
+  },
+  {
+    img: "img/parti66.jpg",
+  },
+  {
+    img: "img/parti67.jpg",
+  },
+  {
+    img: "img/parti68.jpg",
+  },
+  {
+    img: "img/parti69.jpg",
+  },
+  {
+    img: "img/parti70.jpg",
+  },
+];
+
+export default img;
